97c65ac1891b166dfb44487d0611d8a58fe3a238
Alia Reed <alia@fortreed.com>
2020-01-22 09:26:53 -0500

    finished java core assignment

--- CoreJavaAssignment/src/com/revature/javacore/QuestionNumberOne.java
+++ CoreJavaAssignment/src/com/revature/javacore/QuestionNumberOne.java
@@ -1,15 +1,48 @@
 package com.revature.javacore;
 
-public class QuestionNumberOne {
+public class JavaCoreAssignment {
 	
-	void michaelBuble(int aliasArray[]) {	
+	// declare aliasArray
+	static int aliasArray[] = {1, 0, 5, 6, 3, 2, 3, 7, 9, 8, 4};
 		
-		for (int i = 0; i < aliasArray.length; i++) {
+	public static void main(String[] args) {
 		
+		// call Q#1
+		michaelBuble(aliasArray);
+								
+	}
+	
+	static void michaelBuble(int []aliasArray) {
 		
+			// iterate through aliasArray
+			for (int i = 0; i < aliasArray.length; i++) {
+				// second iteration
+				for (int j = 0; j < (aliasArray.length - i - 1); j++) {
+					// comparison statement
+					if (aliasArray[j] > aliasArray[j + 1]) {
+						// is j < or > than j + 1?
+						int tempyTemp = aliasArray[j];
+						aliasArray[j] = aliasArray[j + 1];
+						aliasArray[j+1] = tempyTemp;
+
+					}
+				}
+				
+				// print array
+				System.out.print(aliasArray[i] + " ");
+
+			}
+			// print happy statement
+			System.out.println("\nWe're bubble sorted! Yay!");
+			
 		}
+	
+	static void fFFiboNaCcI() {
+		
+		int fnum = 0;
+		int snum = 1;
 		
 		
 	}
-
+	
 }
